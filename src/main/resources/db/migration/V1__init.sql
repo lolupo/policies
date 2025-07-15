54c39100d3b5e5d476b72df28298c2b0fc957418
Lolupo <lolupo@users.noreply.github.com>
2025-07-16 01:52:37 +0200

    feat(Database): create insurance_policy table and insert sample data

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS insurance_policy
+(
+    id
+    SERIAL
+    PRIMARY
+    KEY,
+    name
+    VARCHAR
+(
+    255
+) NOT NULL,
+    status VARCHAR
+(
+    20
+) NOT NULL,
+    coverage_start_date DATE NOT NULL,
+    coverage_end_date DATE NOT NULL,
+    creation_date DATE NOT NULL,
+    update_date DATE NOT NULL
+    );
